791884ff31568ba0fb628975dbe25f3db035e1f4
abdullahhashim591-max <abdullahhashim591@gmail.com>
2026-06-08 13:12:23 +0500

    gradle files added

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -9,19 +9,16 @@ class DioClient {
   Dio getDio() {
     final dio = Dio(
       BaseOptions(
-        baseUrl: "http://192.168.100.69:5000/api/",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        baseUrl: "http://192.168.1.106:5000/api/",
+        headers: {"Content-Type": "application/json"},
       ),
     );
 
     dio.interceptors.add(
       InterceptorsWrapper(
         onRequest: (options, handler) async {
-
           // skip auth APIs
-          if  (!options.path.contains("login") &&
+          if (!options.path.contains("login") &&
               !options.path.contains("signup") &&
               !options.path.contains("send-otp") &&
               !options.path.contains("verify-otp") &&
@@ -30,12 +27,10 @@ class DioClient {
               !options.path.contains("reset-password") &&
               !options.path.contains("google-login") &&
               !options.path.contains("facebook-login")) {
-
             final token = _localDataSource.getToken();
 
             if (token != null) {
-              options.headers['Authorization'] =
-              'Bearer $token';
+              options.headers['Authorization'] = 'Bearer $token';
             }
           }
 
